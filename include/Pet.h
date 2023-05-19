1a1979ff3f41c61c5a769f4c675629e1f7c20204
erikbuck <erik.buck@wright.edu>
2023-05-18 20:43:33 -0400

    Uncommented code and reformatted

--- include/Pet.h
+++ include/Pet.h
@@ -6,22 +6,16 @@
 /// This class encapsulates information about pets
 class Pet {
 public:
-    uint8_t m_age;
-    const char* m_name;
+  uint8_t m_age;
+  const char *m_name;
 
-    Pet()
-        : m_age(0)
-        , m_name("Anonymous")
-    {
-        // Intentionally empty
-    }
+  Pet() : m_age(0), m_name("Anonymous") {
+    // Intentionally empty
+  }
 
-    Pet(uint8_t age, const char* name)
-        : m_age(age)
-        , m_name(name)
-    {
-        // Intentionally empty
-    }
+  Pet(uint8_t age, const char *name) : m_age(age), m_name(name) {
+    // Intentionally empty
+  }
 };
 
 #endif // Pet_H
--- src/memoryoperations.cpp
+++ src/memoryoperations.cpp
@@ -2,20 +2,19 @@
 #include "Pet.h"
 
 ///////////////////////////////////////////////////////////////////////////////
-Pet* MemoryOperations::memmove(
-    Pet* destination_p, const Pet* source_p, size_t count)
-{
-    if (destination_p > source_p) {
-//        for (int32_t i = count - 1; i >= 0; --i) {
-//            destination_p[i] = source_p[i];
-//        }
-
-    } else {
-        Pet* current_p = destination_p;
-        const Pet* sourceEnd_p = source_p + count;
-        while (source_p < sourceEnd_p) {
-            *current_p++ = *source_p++;
-        }
+Pet *MemoryOperations::memmove(Pet *destination_p, const Pet *source_p,
+                               size_t count) {
+  if (destination_p > source_p) {
+    for (int32_t i = count - 1; i >= 0; --i) {
+      destination_p[i] = source_p[i];
     }
-    return destination_p;
+
+  } else {
+    Pet *current_p = destination_p;
+    const Pet *sourceEnd_p = source_p + count;
+    while (source_p < sourceEnd_p) {
+      *current_p++ = *source_p++;
+    }
+  }
+  return destination_p;
 }
