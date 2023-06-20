18a906414c3c3fdab38c31b45235c1896cb5c6bd
Erik M. Buck <erik.buck@wright.edu>
2023-06-20 14:58:36 -0400

    R3_0 correct compile error

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -24,7 +24,7 @@ TEST(R1_0, moreThanZeroCharacters)
 /// \test @ref R3_0
 TEST(R3_0, scriptInsertCharacter)
 {
-    WSU::Model::StoredString::registerDefaultCommands();
+    //WSU::Model::StoredString::registerDefaultCommands();
     std::shared_ptr<WSU::Model::StoredString> ss_p {
         new WSU::Model::StoredString { "Hello, World!" }
     };
